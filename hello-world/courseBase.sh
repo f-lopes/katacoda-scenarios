6c4682adb4bd6d9fbfaf84256e46d5c778632025
Florian Lopes <florian.lopes@outlook.com>
2017-02-20 19:32:08 +0100

    Revert backend to docker

--- hello-world/courseBase.sh
+++ hello-world/courseBase.sh
@@ -1,3 +1,6 @@
 docker pull flopes/spring-boot-docker:1.0
 
 git clone https://github.com/f-lopes/spring-boot-example .
+
+# Workaround for docker backend image
+docker run --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3.9-jdk-8-alpine mvn
