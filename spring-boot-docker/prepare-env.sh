b55b438c9822bc4323193f652f3bbaff800ca978
Florian Lopes <florian.lopes@outlook.com>
2017-08-14 18:41:56 +0200

    Cache Maven dependencies

--- spring-boot-docker/prepare-env.sh
+++ spring-boot-docker/prepare-env.sh
@@ -4,8 +4,14 @@
 docker pull flopes/spring-boot-docker:1.0
 docker pull maven:3.3.9-jdk-8-alpine
 
+# Maven repository directory
+mkdir /.m2
+
 git clone https://github.com/f-lopes/spring-boot-example
 
 cd spring-boot-example/
+
+### Caching maven dependencies to fasten the package phase
+docker run --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven -v "/.m2":/root/.m2 maven:3.3.9-jdk-8-alpine mvn verify
 
 ### Your environment is now ready!
--- spring-boot-docker/step4.md
+++ spring-boot-docker/step4.md
@@ -2,4 +2,6 @@ Run the Docker image
 
 You can run the previously built image using `docker run -d -p 80:8080 spring-boot`{{execute}}
 
-Run `curl http://docker:8080/home`{{execute}} to see the Spring Boot application running.
+Run `docker ps` to verify your application is running.
+
+Run `curl http://localhost/home`{{execute}} to see the Spring Boot application running.
